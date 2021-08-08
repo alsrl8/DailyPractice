6c61014e84bacbc4398204da1bff7bb0bf780f29
alsrl8 <55830760+alsrl8@users.noreply.github.com>
2021-08-09 02:40:44 +0900

    Update 1068.py

--- Problems/baekjoon/1068.py
+++ Problems/baekjoon/1068.py
@@ -5,11 +5,13 @@ N = int(sys.stdin.readline())
 parents_info = list(map(int, sys.stdin.readline().split()))
 remove = int(sys.stdin.readline())
 
+# initialize root
 root = 0
 for node in range(N):
     if parents_info[node] == -1:
         root = node
 
+# initialize children_info
 children_info = [[] for i in range(N)]
 for i in range(N):
     parent = parents_info[i]
@@ -21,6 +23,7 @@ parent = parents_info[remove]
 if remove != root:
     children_info[parent].remove(remove)
 
+# execute dfs to search tree nodes
 answer = 0
 def dfs(node:int):
     global answer
